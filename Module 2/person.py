6044e2d2ec262aef10cf144a0692554c51f07838
ogulcan dinc <you@example.com>
2022-09-15 15:29:33 +0200

    'mapje'

---
+++ Module 2/person.py
@@ -0,0 +1,14 @@
+from unicodedata import name
+
+
+Name = input('wat jou naam')
+Adres = input('wat is je adres')
+Postcode = input('wat is je postcode')
+Woonplaats = input('wat is je woonplaats')
+
+print('-----------------------------------------')
+print('|     Name :' + Name)
+print('|     Adres :' + Adres)
+print('|     Postcode  :' + Postcode)
+print('|     Woonplaats :' + Woonplaats)
+print('---------------------------------------')
