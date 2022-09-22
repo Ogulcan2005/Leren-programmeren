291af570c48d1e519b06203c8d343f23d7502953
ogulcan dinc <you@example.com>
2022-09-22 13:19:17 +0200

    ''

--- Challenges/smartphone.py
+++ Challenges/smartphone.py
@@ -1,13 +1,13 @@
-iphone_13 = 909
-samsung_s22 = 870
-prijs_verschil = 870 - 768
+prijs_iphone_13 = int(input('prijs van iphone'))
+prijs_samsung_s22 = int(input('prijs van samsung'))
+prijs_asus_zenfone_9 = int(input('prijs van asus'))
+prijs_verschil = (prijs_samsung_s22 - prijs_asus_zenfone_9)
 telefoon = str('iphone')
 telefoon_2 = str('samsung')
-asus_zenfone_9 = 768
 telefoon3 = str('asus zenfone 9')
-prijs_verschil2 = iphone_13 - asus_zenfone_9
+prijs_verschil2 = prijs_iphone_13 - prijs_asus_zenfone_9
 
-print(f'de {telefoon} is het duurst, de telefoon kost: €{iphone_13} ')
-print(f'de {telefoon3} is het goedkoopst, de telefoon kost: €{asus_zenfone_9}')
-print(f'de {telefoon_2} zit er tussenin met: €{samsung_s22}')
+print(f'de {telefoon} is het duurst, de telefoon kost: €{prijs_iphone_13} ')
+print(f'de {telefoon3} is het goedkoopst, de telefoon kost: €{prijs_asus_zenfone_9}')
+print(f'de {telefoon_2} zit er tussenin met: €{prijs_samsung_s22}')
 print(f'het advies is dus de {telefoon3} te kopen deze is namelijk €{prijs_verschil} goedkoper dan de {telefoon_2} en €{prijs_verschil2} goedkoper dan de {telefoon} ')
