b9446d659750bafcac4ef3de7ef3fadc0a73421a
ogulcan dinc <you@example.com>
2022-09-22 10:08:59 +0200

    ''

---
+++ Voor mezelf/smartphone challenge.py
@@ -0,0 +1,9 @@
+iphone_13 = 909
+samsung_s22 = 870
+prijs_verschil = (iphone_13 - samsung_s22)
+telefoon = str('iphone')
+telefoon_2 = str('samsung')
+
+print(f'de {telefoon} is het duurst, de telefoon kost: €{iphone_13} ')
+print(f'de {telefoon_2} is het goedkoopst, de telefoon kost: €{samsung_s22}')
+print(f'het advies is dus geen telefoon te kopen, ze zijn te duur')
