1e2763b2f7f93fd7444d5c058013d39a60dd3a6e
ogulcan dinc <you@example.com>
2022-09-22 09:51:53 +0200

    'smartphone'

---
+++ Module 2/Kaas, kaas en nog eens kaas!.py
@@ -0,0 +1,18 @@
+gele_kaas = input('is de kaas geel?')
+if gele_kaas == 'ja':
+    gaten = input('zitten er gaten in')
+    if gaten == 'ja':
+        duur = input('is de kaas belagelijk duur')
+        else gaten == 'nee'
+             
+        if duur == 'ja':
+            print(' dan is de kaas emmenthaler')
+
+
+    
+
+
+
+
+
+
