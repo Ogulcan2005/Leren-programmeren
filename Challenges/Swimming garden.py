c9118d7de5bd9737ab40b0665ce2256dfa130564
ogulcan dinc <you@example.com>
2022-10-13 09:39:30 +0200

    ''

--- Challenges/Swimming garden.py
+++ Challenges/Swimming garden.py
@@ -1,5 +1,9 @@
-bad_grootte = 8 * 3 * 1,5
+bad_grootte = 8 * 3 * 1.5
 print(bad_grootte)
 m3 = 36
 uitgraven = 25 * m3
-print(uitgraven)
+print(f'{uitgraven}')
+afvoergrond = 32.50 * 36
+print(f'{afvoergrond}')
+totaal = afvoergrond + uitgraven
+print(f'€{totaal}')
