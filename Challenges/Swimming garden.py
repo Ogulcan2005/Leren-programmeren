72968cc5868b024f3f609149c85e572735ecf19c
ogulcan dinc <you@example.com>
2022-10-13 10:01:15 +0200

    ''

--- Challenges/Swimming garden.py
+++ Challenges/Swimming garden.py
@@ -1,9 +1,20 @@
-bad_grootte = 8 * 3 * 1.5
-print(bad_grootte)
-m3 = 36
-uitgraven = 25 * m3
+# stap 1
+zwembad_groote_en_diepte = 8 * 3 * 1.5
+print(zwembad_groote_en_diepte)
+################################################
+# stap 2
+uitgraven = 25 * zwembad_groote_en_diepte
 print(f'{uitgraven}')
 afvoergrond = 32.50 * 36
 print(f'{afvoergrond}')
 totaal = afvoergrond + uitgraven
 print(f'€{totaal}')
+##########################################################
+# stap 3
+voorrijkosten = 250 + float(2.05 * 60)
+totaal2 = (f'€{uitgraven + afvoergrond + voorrijkosten}')
+print(totaal2)
+#######################################
+# stap 4
+vierkante = 8 * 3
+print(vierkante)
