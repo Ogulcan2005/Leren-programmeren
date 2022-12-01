827e8f3aa8c3a5659bc785811fd7a99d8d96b3ed
ogulcan dinc <you@example.com>
2022-12-01 11:39:03 +0100

    ''

---
+++ Module 4/dagen.py
@@ -0,0 +1,18 @@
+alle = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag']
+for s in range(7):
+    print(alle[s])
+print()
+for x in range(5):
+   print(alle[x])
+print()
+for a in range(5,7):
+    print(alle[a])
+print()
+for z in range(6, -1, -1):
+    print(alle[z])
+print()
+for e in range(4, -1, -1):
+    print(alle[e])
+print()
+for u in range(6, 4, -1):
+    print(alle[u])
---
+++ Voor mezelf/tuple.py
@@ -0,0 +1,10 @@
+a = [0, 1, 2, 3, 4, 5]
+for x in a:
+    print(a[x])
+
+if 3 in a:
+    print("Jojo Fucking Sucks")
+else:
+    print('beide')
+
+print(list(range(10, 0, -1)))
