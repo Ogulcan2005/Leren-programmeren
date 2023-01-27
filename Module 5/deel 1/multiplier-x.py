5ea491fbd42b9df9c3a23eb94845e2150dd3fcb9
ogulcan dinc <you@example.com>
2023-01-27 09:56:56 +0100

    ''

--- Module 5/deel 1/multiplier-x.py
+++ Module 5/deel 1/multiplier-x.py
@@ -1,8 +1,5 @@
-# def multiplier(cijfer: int):
-#     cijfer = int(input('hoeveel wilt u'))
-#     for x in range(1, 11):
-#         print(f'{x}x{cijfer} = {x * cijfer}')
-# multiplier(10)
-
-denji = 'Ogulcan is denji en menno is power tijdens de verjaardags taart incident'
-print(denji)
+def multiplier(cijfer: int):
+    cijfer = int(input('hoeveel wilt u'))
+    for x in range(1, 11):
+        print(f'{x}x{cijfer} = {x * cijfer}')
+multiplier(10)
