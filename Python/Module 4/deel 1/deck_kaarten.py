d49201529145acd756ac078d5e57e58ed827f3df
ogulcan dinc <you@example.com>
2023-03-17 09:14:10 +0100

    ''

--- Python/Module 4/deel 1/deck_kaarten.py
+++ Python/Module 4/deel 1/deck_kaarten.py
@@ -2,14 +2,19 @@ from random import shuffle
 kleur = ['harten', 'klaveren', 'schoppen', 'ruiten']
 kaarten = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'boer', 'vrouw', 'heer', 'aas']
 deck = []
+
 for x in range(4):
     for y in range(13):
         deck.append(kleur[x] + ' ' + str(kaarten[y]))
+
 for z in range(2):
     deck.append('joker')
+
 shuffle(deck)
 print(deck)
+
 for a in range(1, 8):
     print(f'kaart {a}: {deck[0]}')
     deck.pop(0)
+
 print(deck)
--- javauileg/script.js
+++ javauileg/script.js
@@ -1,31 +1,39 @@
-let loggedin = []
+let loggedin = [];
 function getindexofelementbyname(lijst,naam){
-    return-1;
-}
+    for (let x = 0; x < lijst.length; x++ ){
+        if (lijst[x]['naam'] === naam){
+            return x;
+        }
+    return null;
+}}
 
 function handelInloggen(){
-    console.log(loggedin)
-    console.log(Date())
+    console.log(loggedin);
+    console.log(Date());
 
-    let naam = document.getElementById('inputnaam').value;
+    const naam = document.getElementById('inputnaam').value;
     if (naam.length <=0){
-        alert('voer een naam in')
-        return
+        alert('voer een naam in');
+        return;
     }
-    console.log('u hebt op de button geklikt')
-    let positie = getindexofelementbyname(loggedin,naam)
-    if (positie > -1){
-        loggedin.splice(loggedin.indexOf(naam), 1);
-        console.log('u bent uitgelogd')
+    console.log('u hebt op de button geklikt');
+
+    const positie = getindexofelementbyname(loggedin,naam);
+    if (positie !== null){
+        const olddate = loggedin[positie]['moment'];
+        const newDate = new Date();
+        const verschil = olddate - newDate / 1000
+        console.log(verschil)
+        loggedin.splice(positie, 1);
+        console.log('u bent uitgelogd');
         document.getElementById('melding').innerText = 'u bent uitgelogd ' + naam;
-        document.getElementById('inputnaam').value = '';
     } else {
         loggedin.push({'naam' : naam, 'moment' : new Date()});
         console.log('u bent ingelogd');
         document.getElementById('melding').innerText = 'u bent ingelogd' + naam;
     }
 document.getElementById('inputnaam').value = '';
-console.log(loggedin)
+console.log(loggedin);
     // console.log('u hebt ingelogd: ' + naam)
     // document.getElementById('inputnaam').value = '';
     // document.getElementById('melding').innerText = 'gelukt';
