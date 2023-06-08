f4f73062819b199342174caf34a63fc91de42e84
ogulcan dinc <you@example.com>
2023-06-08 11:00:22 +0200

    ''

--- Challenges/assesments/memory/memory.js
+++ Challenges/assesments/memory/memory.js
@@ -1,24 +1,35 @@
-const image1 = new Image();
-const image2 = new Image();
-const image3 = new Image();
-const image4 = new Image();
-const image5 = new Image();
-const image6 = new Image();
-const image7 = new Image();
-const image8 = new Image();
-const image9 = new Image();
-const image10 = new Image();
-
+let image1 = document.createElement("img");
 image1.src = "audi.png";
+
+let image2 = document.createElement("img");
 image2.src = "auto.png";
+
+let image3 = document.createElement("img");
 image3.src = "bmw.png";
+
+let image4 = document.createElement("img");
 image4.src = "bugatti.png";
+
+let image5 = document.createElement("img");
 image5.src = "ferrari.png";
+
+let image6 = document.createElement("img");
 image6.src = "fiat.png";
+
+let image7 = document.createElement("img");
 image7.src = "lambo.png";
+
+let image8 = document.createElement("img");
 image8.src = "mercedes.png";
+
+let image9 = document.createElement("img");
 image9.src = "oud.png";
-image10.src = "zentorno.png";
+
+let image10 = document.createElement("img");
+image9.src = "zentorno.png";
+
+
+
 
 function shuffle(array){
     let currentindex = array.length,   randomindex;
@@ -36,4 +47,10 @@ var auto_lijst = [image1, image2, image3, image4, image5, image6, image7, image8
 var auto_lijst2 = shuffle(auto_lijst);
 
 div_bord = document.getElementById("spelbord");
-div_bord.appendChild(auto_lijst);
+
+for (let i = 0; i < auto_lijst.length; i++) {
+    div_bord.appendChild(auto_lijst[i]);
+  }
+
+
+
