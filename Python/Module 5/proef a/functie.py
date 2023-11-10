5619d004bae8841ecb272bf7585fc716a6ff6e55
ogulcan dinc <you@example.com>
2023-11-10 21:43:46 +0100

    ''

--- Python/Module 5/proef a/functie.py
+++ Python/Module 5/proef a/functie.py
@@ -1,28 +1,49 @@
 def aantal_bolletjes() -> int:
-    hoeveel_bollen = int(input("Hoeveel bolletjes wilt u? "))
-    return hoeveel_bollen
+    return int(input("Hoeveel bolletjes wilt u? "))
 
-def bakje_hoorn(bollen):
-    while True:
-        if bollen > 0 and bollen < 4:
-            bakje_hoorn = input(f"Wilt u deze {bollen} bolletje(s) in een hoorntje of een bakje? ")
-            if bakje_hoorn == "bakje":
-                print(f"Hier is uw {bakje_hoorn} met {bollen} bolletje(s).")
-                break
-            elif bakje_hoorn == "hoorn":
-                print(f"Hier is uw {bakje_hoorn} met {bollen} bolletje(s).")
-                break
-            else:
-                print("Sorry, dat snap ik niet...")
-        elif bollen >= 4 and bollen < 9:
-            print(f"Dan krijgt u van mij een bakje met {bollen} bolletjes.")
-            print(f"Hier is uw bakje met {bollen} bolletje(s).")
-            break
-
-        elif bollen >= 9:
-            print("Sorry, zulke grote bakken hebben we niet.")
-            bollen = int(input("Hoeveel bolletjes wilt u? "))
-
-        else:
+def bakje_hoorn(bollen) -> str:
+    if bollen < 4:
+        bakjeofhoorn = input(f"Wilt u deze {bollen} bolletje(s) in een hoorntje of een bakje? ")
+        while bakjeofhoorn not in ["bakje", "hoorn"]:
             print("Sorry, dat snap ik niet...")
-            bollen = aantal_bolletjes()
+            bakjeofhoorn = input(f"Wilt u deze {bollen} bolletje(s) in een hoorntje of een bakje? ")
+        print(f"Hier is uw {bakjeofhoorn} met {bollen} bolletje(s).")
+        return bakjeofhoorn
+    elif bollen < 9:
+        print(f"Dan krijgt u van mij een bakje met {bollen} bolletjes.")
+        print(f"Hier is uw bakje met {bollen} bolletje(s).")
+        return "bakje"
+    else:
+        print("Sorry, zulke grote bakken hebben we niet.")
+
+def bestelling():
+    Bolletjes = 1.10
+    Horrentjes = 1.25
+    Bakjes = 0.75
+    opnieuw = "ja"
+
+    totaal_bollen = 0
+    totaal_bakje = 0
+    totaal_hoorn = 0
+
+    while opnieuw == "ja":
+        aantal_bollen = aantal_bolletjes()
+        bakjeofhoorn = bakje_hoorn(aantal_bollen)
+        totaal_bollen += aantal_bollen
+        if bakjeofhoorn == "bakje":
+            totaal_bakje += 1
+        elif bakjeofhoorn == "hoorn":
+            totaal_hoorn += 1
+        opnieuw = input("Wilt u nog meer bestellen? (ja/nee) ")
+
+    if opnieuw == "nee":
+        print("Uw bestelling:")
+        print("----[Papi Gelato]----")
+        print(f"bolletjes {totaal_bollen} x {Bolletjes}: {totaal_bollen * Bolletjes:.2f}")
+        print(f"bakjes {totaal_bakje} x {Bakjes}:  {totaal_bakje * Bakjes}")
+        print(f"hoorntjes {totaal_hoorn} x {Horrentjes}:  {totaal_hoorn * Horrentjes}")
+        totale_prijs = (totaal_bollen * Bolletjes) + (totaal_bakje * Bakjes) + (totaal_hoorn * Horrentjes)
+        print(f"Totale prijs: €{totale_prijs:.2f}")
+        print("Bedankt en tot ziens")
+
+
--- Python/Module 5/proef a/ijssalon.py
+++ Python/Module 5/proef a/ijssalon.py
@@ -1,17 +1,9 @@
-from functie import aantal_bolletjes, bakje_hoorn
+from functie import *
 
 print("Welkom bij Papi Gelato, je mag alle smaken kiezen zolang het maar vanille ijs is.\n")
-opnieuw = "ja"
-while opnieuw == "ja":
-    bollen = aantal_bolletjes()
-    bakje_hoorn(bollen)
-    opnieuw = input("Wilt u nog meer bestellen? (ja/nee): ")
-    if opnieuw == "nee":
-        print("Bedankt en tot ziens")
-        break
-    else:
-        print("sorry dat snap ik niet.")
-        opnieuw = "ja"
+bestelling()
+
+
         
 
 
